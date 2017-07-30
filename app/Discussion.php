69fe2bb557c407a5a54af73af49243133c0f83aa
sultana <ssazeda@gmail.com>
2017-07-30 14:06:11 +0600

    addming necessary models

---
+++ app/Discussion.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Discussion extends Model
+{
+    protected $fillable=['title','content','user_id','channel_id'];
+    
+    public function channel(){
+        return $this->belongTo('App\Channel');
+    }
+    
+    public function user(){
+        return $this->belongTo('App\User');
+    }
+    
+    public function replies()
+    {
+        return $this->hasMany('App\Reply');
+    }
+}
